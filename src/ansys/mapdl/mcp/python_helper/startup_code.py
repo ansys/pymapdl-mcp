3a11053653921992e4165256293f79e5083adcdc
Camille Latapie <78221213+clatapie@users.noreply.github.com>
2026-04-03 17:33:17 +0100

    feat: update pre-commit with best practices (#106)
    * feat: add ``typos`` and ``ruff``
    
    * fix: removing comments in json file for ``check json``
    
    * fix: imports
    
    * fix: pre-commit
    
    * fix: show pip list
    
    * fix: show pip list
    
    * fix: ignoring ``PTH123``
    
    * docs: adding comment to ignore check
    
    * revert: readding comments in json file
    
    * chore: adding changelog file 106.added.md [dependabot-skip]
    
    * fix: typos
    
    ---------
    
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/mapdl/mcp/python_helper/startup_code.py
+++ src/ansys/mapdl/mcp/python_helper/startup_code.py
@@ -1,11 +1,35 @@
+# Copyright (C) 2025 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Startup code for PyMAPDL MCP."""
+
 import base64
-import sys
 from io import BytesIO, TextIOWrapper
+import sys
 
 import matplotlib
 import matplotlib.pyplot as plt
-import pyvista as pv
 from PIL import Image
+import pyvista as pv
 
 # Set UTF-8 encoding for stdout and stderr to handle Unicode characters
 if sys.stdout.encoding != "utf-8":
